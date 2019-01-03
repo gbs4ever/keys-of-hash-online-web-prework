2243408089c846c88b48689aeb56aa9a85c98a90
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-03 04:30:25 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
+require 'pry'
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
+  def keys_of(*args)
+    self.map do |key, value|
+      if args.include?(value)
+        key
+      else
+        nil
+
+      end
+    end.compact
+end
 end
